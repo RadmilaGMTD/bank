5ec376c75861f685885f3a438860e0bd66384183
Radmila Gimatdinova <raf1507@mail.ru>
2024-10-19 03:23:53 +0400

    add logs

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,22 @@
 import json
 from json import JSONDecodeError
 from typing import Any
+import logging
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/utils.log','w',encoding='utf-8')
+file_formate = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formate)
+logger.addHandler(file_handler)
 
 
 def file_read(file: Any = "C:/Users/VIP/my_prj/bank/data/operations.json") -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
     try:
+        logger.info(f'Проверяем наличие файла {file}')
         if file:
+            logger.info(f'Записываем в файл {file}')
             with open(file, "r", encoding="utf-8") as f:
                 operations = json.load(f)
                 if len(operations) == 0 or not isinstance(operations, list):
@@ -15,5 +25,6 @@ def file_read(file: Any = "C:/Users/VIP/my_prj/bank/data/operations.json") -> li
         if not file:
             return []
     except (FileNotFoundError, JSONDecodeError):
+        logger.error('Файл не найден')
         return []
     return []  # Добавила, чтобы mypy не показывал ошибку
