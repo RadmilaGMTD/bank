b71865a146e6346f1ae4a6ce713acb662347c635
Radmila Gimatdinova <raf1507@mail.ru>
2024-10-27 19:19:27 +0400

    update filter_count.py

--- src/filter_count.py
+++ src/filter_count.py
@@ -3,12 +3,14 @@ from collections import Counter
 
 
 def filter_transaction(list_dict: list, description_value: str) -> list:
+    """Функция для поиска в списке словарей операций по заданной строке"""
     result = [description for description in list_dict if re.match(description_value, description["description"])]
     return result
 
 
 def count_transaction(list_dict: list, list_description: list) -> dict:
+    """Функция для подсчета количества банковских операций определенного типа."""
     category = [
         description["description"] for description in list_dict if description["description"] in list_description
     ]
-    return Counter(category)
+    return dict(Counter(category))
